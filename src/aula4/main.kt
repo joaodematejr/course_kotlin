0bb58d991ea700f0e58c2d59c51313126ce83e99
João Dematé Jr <joaodematejr@gmail.com>
2022-07-07 17:33:58 -0300

    Aula04

---
+++ src/aula4/main.kt
@@ -0,0 +1,18 @@
+
+fun main() {
+    /*
+    Operadores Aritméticos
+    + Adição
+    - Subtração
+    '*' Multiplicação 
+    / Divisão
+    % Módulo (resto da divisão)
+    ++ Incremento
+    -- Decremento
+    */
+
+    var numberOne = 20
+    var numberTwo = 10
+
+    println(numberOne / numberTwo)
+}
