815f858eb6d23a2d737a0b9c1a90b46a1dbb065f
João Dematé Jr <joaodematejr@gmail.com>
2022-07-07 17:21:36 -0300

    Atualização aula 2

---
+++ src/aula2/main.kt
@@ -0,0 +1,26 @@
+
+fun main() {
+    /*
+    String - Textos
+    byte - Números Inteiros (-128 a 127)
+    Int - Numeros Inteiros (-2147483648 a 2147483647)
+    long - Numeros Inteiros (-9223372036854775808 a 9223372036854775807)
+    float - Numeros Decimais (1.1234567 até 7 casas decimais)
+    double - Numeros Decimais (1.1234567 até 15 casas decimais)
+    boolean - Verdadeiro ou Falso - true ou false
+    */
+
+    /* val nomeCompleto: String = "João Dematé"
+    val idade: Byte = 20 */
+
+
+    val nomeCompleto: String = "João Dematé"
+    val idade: Byte = 20
+    val numberLong: Long = 1234567890123456789
+    val numberFloat: Float = 1.1234567F
+    val numberDouble: Double = 1.1234567890123456789
+    val isTrue: Boolean = true
+    
+
+    print(idade)
+}
