12787f022faf177b3c6cc6dd2d0c71bbb40b12dd
João Dematé Jr <joaodematejr@gmail.com>
2022-07-07 17:27:56 -0300

    aula3

---
+++ src/aula3/main.kt
@@ -0,0 +1,14 @@
+
+fun main() {
+    /*
+    Concatenação 
+    */
+    var name = "João Dematé"
+    var age = 30
+    var profile = "Software Developer"
+
+    //PRINT IN JAVA
+    //print("Meu nome é " + name + " eu tenho " + age + " anos e sou " + profile)
+    //PRINT IN KT
+    println("Meu nome é $name eu tenho $age anos e sou $profile")
+}
