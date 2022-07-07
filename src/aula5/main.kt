22d782e8503562c1dc4b21bff99328d43319741f
João Dematé Jr <joaodematejr@gmail.com>
2022-07-07 17:43:52 -0300

    aula05

---
+++ src/aula5/main.kt
@@ -0,0 +1,17 @@
+
+fun main() {
+    /*
+    Operadores Relacionais
+    == Igual
+    > Maior que
+    < Menor que
+    >= Maior ou igual
+    <= Menor ou Igual
+    '!=' Diferente
+    */
+
+    var valueOne = 20
+    var valueTwo = 10
+
+    println(valueOne == valueTwo)
+}
