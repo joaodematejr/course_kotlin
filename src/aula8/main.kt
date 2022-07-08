ae134260bd80b3d877b258dfdb5ef459909971c4
João Dematé Jr <joaodematejr@gmail.com>
2022-07-08 15:58:02 -0300

    aula8

---
+++ src/aula8/main.kt
@@ -0,0 +1,13 @@
+
+fun main(args: Array<String>) {
+    /*
+    Arrays are a collection of items that are stored in contiguous memory locations.
+    */
+    //array list product
+    var arrayList = arrayOf("Arroz", "Feijão", "Açucar", "Leite", "Ovos", "Carne", "Manteiga", "Alface", "Tomate", "Cenoura", "Abacaxi", "Banana", "Abacate")
+    var listNumber = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+    
+    //println(Arrays.toString(arrayList))
+    //println(arrayList.size)
+
+}
