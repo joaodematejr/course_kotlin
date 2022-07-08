61299af30be833bcb6c38b80a174559ce089ba77
João Dematé Jr <joaodematejr@gmail.com>
2022-07-08 15:46:16 -0300

    aula07

---
+++ src/aula7/main.kt
@@ -0,0 +1,30 @@
+
+fun main() {
+    /*
+    Operadores Lógicos
+
+    && -> AND -> E -> Se ambos os lados forem verdadeiros
+    || -> OR -> OU -> Se um dos lados for verdadeiro
+    '!' -> NOT -> NÃO -> Se o lado for falso
+
+     */
+    */
+    // True && True -> True
+    if (20 == 20 && 10 < 20) {
+       println("True")
+    } else {
+        println("False")
+    }
+
+    
+    // True || False -> True    
+    // False || False -> False    
+    // True || True -> True
+    if (20 == 20 && 10 < 20) {
+       println("True")
+    } else {
+        println("False")
+    }
+
+
+}
