83a1fe956d72e3ee1a34fe724d05bb0245aca55b
João Dematé Jr <joaodematejr@gmail.com>
2022-07-08 15:34:38 -0300

    Aula06

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Curso de Kotlin
 
 Run cmd kotlinc main.kt -include-runtime -d main.jar && java -jar main.jar
+
+https://www.youtube.com/watch?v=JmEgTuF6ETM&list=PLizN3WA8HR1z1HJy0uSjNe4zhmzJNplb3&index=6
---
+++ src/aula6/main.kt
@@ -0,0 +1,21 @@
+
+fun main() {
+    /*
+    Estrutura Condicional if/else
+    */
+
+    // Se (if) a condição for verdadeira, executa o bloco de código
+    // Senão (else), executa o bloco de código
+
+    var age = 18
+
+    if (age <= 10) {
+       println("O usuário é uma criança")
+    } else if (age <= 17) {
+       println("O usuário é adolescente")
+    } else {
+        println("O usuário é menor de idade")
+    }
+
+
+}
